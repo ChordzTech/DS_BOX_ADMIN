7f6eab806e43eddcab65e9429209f24609e91e76
Shubham-7741 <shubhamsabale358@gmail.com>
2023-12-15 18:00:07 +0530

    tables ui all forms are resonsive

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -19,32 +19,56 @@ barChartType: any;
     this.chart = new Chart("canvas", {
       type: "bar",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"],
         datasets: [
           {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
+            label: "# of Last week Joinings",
+            data: [10, 14, 5, 4, 6, 17, 7],
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
+              "rgb(134,142,150)"
+              // "rgba(255, 99, 132, 0.2)",
+              // "rgba(54, 162, 235, 0.2)",
+              // "rgba(255, 206, 86, 0.2)",
+              // "rgba(75, 192, 192, 0.2)",
+              // "rgba(153, 102, 255, 0.2)",
+              // "rgba(255, 159, 64, 0.2)"
             ],
             borderColor: [
-              "rgba(255, 99, 132, 1)",
+              "rgba(238,238,238, 1)",
+              // "rgba(54, 162, 235, 1)",
+              // "rgba(255, 206, 86, 1)",
+              // "rgba(75, 192, 192, 1)",
+              // "rgba(153, 102, 255, 1)",
+              // "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          },
+          {
+            label: "# of this week Joinings",
+            data: [12, 19, 3, 5, 2, 3, 9],
+            backgroundColor: [
+              // "rgba(255, 99, 132, 0.2)",
+              // "rgba(54, 162, 235, 0.2)",
+              "rgb(7,157,243)"
+              // "rgba(255, 206, 86, 0.2)",
+              // "rgba(75, 192, 192, 0.2)",
+              // "rgba(153, 102, 255, 0.2)",
+              // "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              // "rgba(255, 99, 132, 1)",
               "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
+              // "rgba(255, 206, 86, 1)",
+              // "rgba(75, 192, 192, 1)",
+              // "rgba(153, 102, 255, 1)",
+              // "rgba(255, 159, 64, 1)"
             ],
             borderWidth: 1
           }
         ]
       },
       options: {
+        maintainAspectRatio:false,
         scales: {
           // yAxes: [
           //   {
